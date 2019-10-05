4affabd484ba5e74288ebc98b251f866376b55d3
Julian Simpson <simpsonjulian@gmail.com>
2019-10-05 17:22:44 +1300

    New month, new batch

--- label.sh
+++ label.sh
@@ -1,2 +1,2 @@
-source="https://live.staticflickr.com/7006/6514352725_3e99b561ed_b_d.jpg"
-curl -o label.jpg https://memegen.link/custom/best_bitter/august_2019.jpg?alt=${source}
+#!/bin/bash
+curl -o label.jpg https://memegen.link/hipster/supermarket_stout/october_2019.jpg
